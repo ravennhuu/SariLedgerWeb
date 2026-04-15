b00bd285daf4a261ea229dd7783fea2976dc619e
Avryl Raven Alamo <avrylalamo@gmail.com>
2026-04-16 03:11:44 +0800

    fix deployment issues

--- sari-ledger-web/vite.config.js
+++ sari-ledger-web/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: 'sari-ledger-web',
+  root: './',
+  build: {
+    outDir: '../dist',
+  },
   plugins: [react()],
 })
