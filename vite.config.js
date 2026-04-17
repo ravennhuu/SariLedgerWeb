df47d8e26295ab96500c61eb04de53130c089c8c
Avryl Raven Alamo <avrylalamo@gmail.com>
2026-04-17 23:34:45 +0800

    update viteConfig2

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     tailwindcss(),
   ],
   build: {
-    outDir: 'public',
+    outDir: 'dist',
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
